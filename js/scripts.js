e972b3f88d4e1b9a18706c33a27aad437087d490
Immagitonga25 <immaculate.gitonga@student.moringaschool.com>
2021-04-12 01:14:43 +0300

    use control flows and logical operators to validate inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,39 @@
 //finding html elements by id
-var centuryBorn = document.getElementById("century").value;
-var yearBorn = document.getElementById("year").value;
-var monthBorn = document.getElementById("month").value;
-var dayBorn = document.getElementById("day").value;
+var cc = parseInt(document.getElementById("century").value);
+var yy = parseInt(document.getElementById("year").value);
+var mm= parseInt(document.getElementById("month").value);
+var dd = parseInt(document.getElementById("day").value);
 var gender = document.getElementById("gender");
+//validating user input
+function monthBorn () {
+    if (mm < 1 ||mm > 12) {
+        alert("Input not valid!");
+        return false;
+    } else {
+        return true;
+    }
+}
+function dayBorn () {
+    if (dd < 01 || dd > 31) {
+        alert("Input not valid!");
+        return false;
+    } else {
+        return true;
+    }
+}
+function centuryBorn () {
+    if (cc < 18 || cc > 20) {
+        alert("Invalid input!")
+        return false;
+    } else {
+        return true;
+    }
+}
+function yearBorn () {
+    if (yy < 00 || yy > 99) {
+        alert("Invalid input!")
+        return false;
+    } else {
+        return true;
+    }
+}
