376e804bb173020b535c2054d630fa74b0447381
Immagitonga25 <immaculate.gitonga@student.moringaschool.com>
2021-04-12 00:42:00 +0300

    find html elements and store them in variables in js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,6 @@
-function day(cc,yy,mm,dd) {return (((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7};
-var cc
+//finding html elements by id
+var centuryBorn = document.getElementById("century").value;
+var yearBorn = document.getElementById("year").value;
+var monthBorn = document.getElementById("month").value;
+var dayBorn = document.getElementById("day").value;
+var gender = document.getElementById("gender");
