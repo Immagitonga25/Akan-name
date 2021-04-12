adf1f1ce22b7ce322e54879b1df5df1a5ec576ed
Immagitonga25 <immaculate.gitonga@student.moringaschool.com>
2021-04-12 12:06:27 +0300

    make changes to html and js files

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,23 @@
-
-//validating user input
-function monthBorn () {
-    if (mm < 1 ||mm > 12) {
+document.getElementById("btn").addEventListener("click", akanName);
+function akanName() {
+    var month= parseInt(document.getElementById("month").value);
+    var day = parseInt(document.getElementById("day").value);
+    var century = parseInt(document.getElementById("century").value);
+    var year = parseInt(document.getElementById("year").value);
+    if (month < 1 ||month > 12) {
         alert("Input not valid!");
         return false;
-    }
-    }
-}
-function dayBorn () {
-    if (dd < 01 || dd > 31) {
+    } else if (day < 01 || day > 31) {
         alert("Input not valid!");
         return false;
-    }
-    }
-}
-function centuryBorn () {
-    if (cc < 18 || cc > 20) {
+    } else if (century < 18 || century > 20) {
         alert("Invalid input!")
         return false;
-    }
-    }
-}
-function yearBorn () {
-    if (yy < 00 || yy > 99) {
+    } else if (year < 00 || year > 99) {
         alert("Invalid input!")
         return false;
-    } 
+    } else {
+        return true;
     }
 }
 function getData () {
@@ -38,34 +30,48 @@ function getData () {
     var dd = parseInt(document.getElementById("day").value);
     var gender = document.getElementById("gender");
     var d = (((cc/4)-2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7;
-    var akanDay = d.toFixed();
+    var akanDay = (d.toFixed());
     if (akanDay === 0 && gender === male) {
-    document.getElementById("result").innerHTML="Your Akan Name is " + maleAkan[0];
+    document.getElementById("output").value = "Your Akan Name is " + maleAkan[0];
+    return true;
     } else if (akanDay === 0 && gender === female) {
-        document.getElementById("result").innerHTML="Your Akan Name is " + femaleAkan[0];
+        document.getElementById("output").value = "Your Akan Name is " + femaleAkan[0];
+        return true;
     } else if (akanDay === 1 && gender === male) {
-        document.getElementById("result").innerHTML="Your Akan Name is " + maleAkan[1];
+        document.getElementById("output").value = "Your Akan Name is " + maleAkan[1];
+        return true;
     } else if (akanDay === 1 && gender === female) {
-        document.getElementById("result").innerHTML="Your Akan Name is " + femaleAkan[1];
+        document.getElementById("output").value = "Your Akan Name is " + femaleAkan[1];
+        return true;
     } else if (akanDay === 2 && gender === male) {
-        document.getElementById("result").innerHTML="Your Akan Name is " + maleAkan[2];
+        document.getElementById("output").value = "Your Akan Name is " + maleAkan[2];
+        return true;
     } else if (akanDay === 2 && gender === female) {
-        document.getElementById("result").innerHTML="Your Akan Name is " + femaleAkan[2];
+        document.getElementById("output").value = "Your Akan Name is " + femaleAkan[2];
+        return true;
     } else if (akanDay === 3 && gender === male) {
-        document.getElementById("result").innerHTML="Your Akan Name is " + maleAkan[3];
+        document.getElementById("output").value ="Your Akan Name is " + maleAkan[3];
+        return true;
     } else if (akanDay === 3 && gender === female) {
-        document.getElementById("result").innerHTML="Your Akan Name is " + femaleAkan[3];
+        document.getElementById("output").value="Your Akan Name is " + femaleAkan[3];
+        return true;
     } else if (akanDay === 4 && gender === male) {
-        document.getElementById("result").innerHTML="Your Akan Name is " + maleAkan[4];
+        document.getElementById("output").value="Your Akan Name is " + maleAkan[4];
+        return true;
     }else if (akanDay === 4 && gender === female) {
-        document.getElementById("result").innerHTML="Your Akan Name is " + femaleAkan[4];
+        document.getElementById("output").value="Your Akan Name is " + femaleAkan[4];
+        return true;
     }else if (akanDay === 5 && gender === male) {
-        document.getElementById("result").innerHTML="Your Akan Name is " + maleAkan[5];
+        document.getElementById("output").value="Your Akan Name is " + maleAkan[5];
+        return true;
     }else if (akanDay === 5 && gender === female) {
-        document.getElementById("result").innerHTML="Your Akan Name is " + femaleAkan[5];
+        document.getElementById("output").value="Your Akan Name is " + femaleAkan[5];
+        return true;
     } else if (akanDay === 6 && gender === male) {
-        document.getElementById("result").innerHTML="Your Akan Name is " + maleAkan[6];
+        document.getElementById("output").value="Your Akan Name is " + maleAkan[6];
+        return true;
     } else if (akanDay === 6 && gender === female) {
-        document.getElementById("result").innerHTML="Your Akan Name is " + femaleAkan[6];
+        document.getElementById("output").value="Your Akan Name is " + femaleAkan[6];
+        return true;
     }
 }
