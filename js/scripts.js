2670e6df282b282a1ad47a2dfe86f6e3921b0594
Immagitonga25 <immaculate.gitonga@student.moringaschool.com>
2021-04-13 11:40:16 +0300

    final draft

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,6 @@ function akanName() {
     var day = parseInt(document.getElementById("day").value);
     var century = parseInt(document.getElementById("century").value);
     var year = parseInt(document.getElementById("year").value);
-    var gender = document.getElementById("gender").value;
     
     function correctMonth() {
         if (month < 1 ||month > 12) {
@@ -33,57 +32,61 @@ function akanName() {
             return true;
         }
     }
-    var validMonth = correctMonth();
-    var validDay = correctDay();
-    var validYear = correctYear();
-
-    var maleAkan = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
-    var femaleAkan = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
-      
     var dayOfWeek = (((century/4)-2*century-1)+((5*year/4))+((26*(month+1)/10))+day)%7;
-    var akanDay = (dayOfWeek.toFixed());
+    var akanDay = dayOfWeek.toFixed();
     console.log(akanDay);
-    
-    if (akanDay === 0 && gender === "male" && validDay && validMonth && validYear) {
-        document.getElementById("result").value = maleAkan[0];
-        return true;
-    } else if (akanDay === 0 && gender === "female" && validDay && validMonth && validYear) {
-        document.getElementById("result").value = femaleAkan[0];
-        return true;
-    } else if (akanDay === 1 && gender === "male" && validDay && validMonth && validYear) {
-        document.getElementById("result").value = maleAkan[1];
-        return true;
-    } else if (akanDay === 1 && gender === "female" && validDay && validMonth && validYear) {
-        document.getElementById("result").value = femaleAkan[1];
-        return true;
-    } else if (akanDay === 2 && gender === "male" && validDay && validMonth && validYear) {
-        document.getElementById("result").value = maleAkan[2];
-        return true;
-    } else if (akanDay === 2 && gender === "female" && validDay && validMonth && validYear) {
-        document.getElementById("result").value = femaleAkan[2];
-        return true;
-    } else if (akanDay === 3 && gender === "male" && validDay && validMonth && validYear) {
-        document.getElementById("result").value = maleAkan[3];
-        return true;
-    } else if (akanDay === 3 && gender === "female" && validDay && validMonth && validYear) {
-        document.getElementById("result").value = femaleAkan[3];
-        return true;
-    } else if (akanDay === 4 && gender === "male" && validDay && validMonth && validYear) {
-        document.getElementById("result").value = maleAkan[4];
-        return true;
-    }else if (akanDay === 4 && gender === "female" && validDay && validMonth && validYear) {
-        document.getElementById("result").value = femaleAkan[4];
-        return true;
-    }else if (akanDay === 5 && gender === "male" && validDay && validMonth && validYear) {
-        document.getElementById("result").value = maleAkan[5];
-        return true;
-    }else if (akanDay === 5 && gender === "female" && validDay && validMonth && validYear) {
-        document.getElementById("result").value = femaleAkan[5];
-        return true;
-    } else if (akanDay === 6 && gender === "male" && validDay && validMonth && validYear) {
-        document.getElementById("result").value = maleAkan[6];
-        return true;
-    } else if (akanDay === 6 && gender === "female" && validDay && validMonth && validYear) {
-        document.getElementById("result").value = femaleAkan[6];
-    } 
+    document.getElementById("btn").addEventListener("click", answer);
+    function answer() {
+        var output;
+        var validMonth = correctMonth();
+        var validDay = correctDay();
+        var validYear = correctYear();
+        var maleAkan = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+        var femaleAkan = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'] 
+        var gender = document.getElementById("gender").value;
+        if (akanDay === 0 && validDay && validMonth && validYear) {
+            output = maleAkan[0];
+            return true;
+        } else if (akanDay === 0 && validDay && validMonth && validYear) {
+            output = femaleAkan[0];
+            return true;
+        } else if (akanDay === 1 && validDay && validMonth && validYear) {
+            output = maleAkan[1];
+            return true;
+        } else if (akanDay === 1 && validDay && validMonth && validYear) {
+            output = femaleAkan[1];
+            return true;
+        } else if (akanDay === 2 && validDay && validMonth && validYear) {
+            output = maleAkan[2];
+            return true;
+        } else if (akanDay === 2 && validDay && validMonth && validYear) {
+            output = femaleAkan[2];
+            return true;
+        } else if (akanDay === 3 && validDay && validMonth && validYear) {
+            output = maleAkan[3];
+            return true;
+        } else if (akanDay === 3 && validDay && validMonth && validYear) {
+            output = femaleAkan[3];
+            return true;
+        } else if (akanDay === 4 && validDay && validMonth && validYear) {
+            output = maleAkan[4];
+            return true;
+        }else if (akanDay === 4 && validDay && validMonth && validYear) {
+            output = femaleAkan[4];
+            return true;
+        }else if (akanDay === 5 && validDay && validMonth && validYear) {
+            output = maleAkan[5];
+            return true;
+        }else if (akanDay === 5 && validDay && validMonth && validYear) {
+            output = femaleAkan[5];
+            return true;
+        } else if (akanDay === 6 && validDay && validMonth && validYear) {
+            output = maleAkan[6];
+            return true;
+        } else if (akanDay === 6 && validDay && validMonth && validYear) {
+            output = femaleAkan[6];
+            return true;
+        } 
+        document.getElementById("result").innerHTML = output
+    }
 }
